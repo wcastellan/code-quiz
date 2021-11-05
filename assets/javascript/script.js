3b606cceeaccd9d77a76c73af546a593afb6c05c
Wyatt C <wcastellan@yahoo.com>
2021-11-05 14:50:38 -0400

    create our questions in javascript

---
+++ assets/javascript/script.js
@@ -0,0 +1,32 @@
+// write our questions
+
+const question = [
+    {
+        question: "Commonly used data types Do Not include:",
+        choices: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+        answer: "4. numbers"
+    },
+    {
+        question: "The condition in an if / else statement is enclosed with ______.",
+        choices: ["1. quotes", "2. curly brackets", "3. parenthesis", "4. square brackets"],
+        answer: "2. curly brackets"
+    },
+    {
+        question: "Arrays in JavaScript can be used to store ______.",
+        choices: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
+        answer: "4. all of the above"
+    },
+    {
+        question: "String values must be enclosed within ______ when being assigned to variables.",
+        choices: ["1. commas", "2. curly brackets", "3. quotes", "4. parenthesis"],
+        answer: "3. quotes"
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ["1. JavaScript", "2. terminal/bash", "3. for loops", "4. console.log"],
+        answer: "4. console.log"
+    }
+];
+
+// create var to make references to elements
+
